aaa23570b66ae4b9e07db83f0dd8de857f23ddfe
abel <raojianli66@163.com>
2020-04-15 08:57:00 +0800

    init

---
+++ doudou/target/debug/deps/example-b3ef2c544d4db526.d
@@ -0,0 +1,5 @@
+D:\myworkspace\doudou\doudou\target\debug\deps\example-b3ef2c544d4db526.exe: tests\example.rs
+
+D:\myworkspace\doudou\doudou\target\debug\deps\example-b3ef2c544d4db526.d: tests\example.rs
+
+tests\example.rs:
---
+++ doudou/tests/example.rs
@@ -0,0 +1,5 @@
+#[test]
+fn example(){
+    let a =3;
+    print!("{}",a)
+}
